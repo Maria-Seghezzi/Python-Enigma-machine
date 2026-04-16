cff84aa7ec8382a28eff48f27387b937e73a3188
Maria <seghezzi.maria@gmail.com>
2026-04-16 19:21:45 +0200

    Defined Reflector class

---
+++ components/reflector.py
@@ -0,0 +1,11 @@
+class Reflector:
+
+    def __init__(self, wiring):
+        self.wiring = wiring
+
+    def encode(self, letter: str):
+        """
+        Encodes letter, returns encoded letter (char)
+        """
+        letter_index = ord(letter) - 65
+        return self.wiring[(letter_index) % 26]
