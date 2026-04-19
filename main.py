ff51819211ecf07afc161811632f89e5a6c8b8a7
Maria <seghezzi.maria@gmail.com>
2026-04-19 20:58:49 +0200

    Updated test file

--- main.py
+++ main.py
@@ -1,80 +1,8 @@
-from config.wiring_data import ROTORS_WIRINGS, REF_WIRING
-from components import Rotor, Reflector, Plugboard
-from machine import Machine
+from interface import get_configuration
+from machine import encode_message
 
 # Future improvements:
 # - Add notchs
-# - Add plugboard
-
-
-def parse_rotors(rotors_string):
-    """
-    Check if input contains valid rotors names and positions and returns list of Rotor objects
-    """
-    valid_input_name = ["I", "II", "III", "IV", "V"]
-
-    # Remove white spaces and create list
-    rotors = rotors_string.replace(" ", "").split(",")
-    # Create rotors list
-    rotors_list = []
-    for r in rotors:
-        if ":" not in r:
-            raise ValueError
-        rotors_list.append(r.split(":"))
-
-    # Check length
-    if len(rotors_list) != 3:
-        raise ValueError
-    # Check if elements are valid names and position, and that there are no duplicates
-    for r in rotors_list:
-        if r[0].upper() not in valid_input_name:
-            raise ValueError
-        valid_input_name.remove(r[0])
-        if int(r[1]) < 0 or int(r[1]) > 26:
-            raise ValueError
-
-    # Create list of Rotor objects
-    rotors_obj = []
-    for i, r in enumerate(rotors_list):
-        rotation_freq = 1 if i == 0 else 26 * i
-        wiring = ROTORS_WIRINGS[r[0]]
-        position = int(r[1])
-        rotors_obj.append(Rotor(wiring, position, rotation_freq))
-    return rotors_obj
-
-
-def parse_ref(ref_string):
-    """
-    Check if input contains valid reflector name and returns reflector object
-    """
-    valid_input = ["A", "B", "C"]
-    if ref_string.upper() not in valid_input:
-        raise ValueError
-    # Create Reflector
-    wiring = REF_WIRING[ref_string]
-    ref_obj = Reflector(wiring)
-    return ref_obj
-
-
-def parse_connections(connection_string):
-    """
-    Check if input contains valid plugboard connections and return Plugboard object
-    """
-    connections_list = connection_string.replace(" ", "").split(",")
-    connections = {}
-    for connection in connections_list:
-        if ":" not in connection:
-            raise ValueError
-        a, b = connection.split(":")
-        a, b = a.upper(), b.upper()
-        if a in connections or b in connections or a == b:
-            raise ValueError
-        connections[a] = b
-        connections[b] = a
-    # Create Plugboard object
-    plugboard_obj = Plugboard(connections)
-
-    return plugboard_obj
 
 
 def print_presentation():
@@ -99,62 +27,10 @@ def print_presentation():
     print("\n")
 
 
-def configuration():
-    """
-    Ask user to define the machine configuration
-    """
-
-    # Get rotors
-    rotors_s = input(
-        "Insert rotors to use and initial positions separated by a comma (eg: I:0, II:1, III:0): "
-    )
-    while True:
-        try:
-            rotors_obj = parse_rotors(rotors_s)
-            break
-        except ValueError:
-            rotors_s = input("Please insert a valid input: ")
-
-    # Get reflector
-    ref_s = input("Insert reflector to use (A, B or C): ").upper()
-    while True:
-        try:
-            ref_obj = parse_ref(ref_s)
-            break
-        except ValueError:
-            ref_s = input("Please insert a valid input: ").upper()
-
-    # Get plugboard connections
-    plug_s = input("Insert plugboard connections separated by a comma (eg A:B, C:D)")
-    while True:
-        try:
-            plugboard_obj = parse_connections(plug_s)
-            break
-        except ValueError:
-            plug_s = input("Please insert a valid input: ")
-
-    # Initializing machine
-    enigma = Machine(rotors_obj, ref_obj, plugboard_obj)
-    print("Machine created!")
-    done = 1
-    return enigma
-
-
-def encode_message(message: str, machine: Machine):
-    message = message.upper()
-    enc_message = ""
-    for letter in message:
-        if letter.isalpha():
-            enc_message += machine.encode(letter)
-        else:
-            enc_message += letter
-    return enc_message
-
-
 def main():
     print_presentation()
     input("Press enter to enter configuration mode")
-    enigma = configuration()
+    enigma = get_configuration()
     message = input("Enter message to encode or decode: ")
     encoded_message = encode_message(message, enigma)
     print("Encoded message:", encoded_message)
--- test/test.py
+++ test/test.py
@@ -1,15 +1,45 @@
-from components.rotor import Rotor
-from components.reflector import Reflector
-from config.wiring_data import *
+from interface import parse_connections, parse_ref, parse_rotors
 from machine import Machine
+from machine import encode_message
+import json
+import copy
 
 word = "HELLO"
 
-# Defining configuration
-rotors = [Rotor(WIRING_I, 0, 0), Rotor(WIRING_I, 1, 0), Rotor(WIRING_I, 2, 0)]
-ref = Reflector(WIRING_R_C)
 
-# Test encoding
-m = Machine(rotors, ref)
-m.print_components()
-print(m.encode("A"))
+# Defining configuration
+def define_machine():
+    with open("test\configuration.json", "r") as config_file:
+        config_data = json.load(config_file)
+        rotors_string = config_data["rotors"]
+        ref_string = config_data["reflector"]
+        plugboard_string = config_data["plugboard"]
+    print(rotors_string, ref_string, plugboard_string)
+    try:
+        rotors = parse_rotors(rotors_string)
+        ref = parse_ref(ref_string)
+        plug = parse_connections(plugboard_string)
+    except ValueError:
+        print("Somthing wrong in configuration")
+    else:
+        machine = Machine(rotors, ref, plug)
+    return machine
+
+
+def test_simmetry(machine: Machine, file_path: str):
+    enc_machine = machine
+    dec_machine = copy.deepcopy(machine)
+    with open(file_path, "r") as file:
+        for string in file:
+            word = string.replace("\r", "").replace("\n", "").upper()
+            enc_message = encode_message(word, enc_machine)
+            dec_message = encode_message(enc_message, dec_machine)
+            assert word == dec_message
+
+
+def test():
+    machine = define_machine()
+    test_simmetry(machine, "test\\test_cases.txt")
+
+
+test()
