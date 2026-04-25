0a9302f8ce9da0d5315c473c359b33b5fcb1d44f
Maria <seghezzi.maria@gmail.com>
2026-04-25 15:56:40 +0200

    changed presentation

--- main.py
+++ main.py
@@ -6,34 +6,26 @@ from machine import encode_message
 
 
 def print_presentation():
-    """
-    Print presentation
-    """
-    presentation = "ENIGMA MACHINE EMULATOR"
-    for j in range(2):
-        for i in range(len(presentation) + 12):
-            print("#", end="")
-        print()
-    for i in range(6):
-        print("-", end="")
-    print(presentation, end="")
-    for i in range(6):
-        print("-", end="")
-    print()
-    for j in range(2):
-        for i in range(len(presentation) + 12):
-            print("#", end="")
-        print()
-    print("\n")
+    presentation = """ _____      _                         _____                _       _             
+|  ___|    (_)                       |  ___|              | |     | |            
+| |__ _ __  _  __ _ _ __ ___   __ _  | |__ _ __ ___  _   _| | __ _| |_ ___  _ __ 
+|  __| '_ \| |/ _` | '_ ` _ \ / _` | |  __| '_ ` _ \| | | | |/ _` | __/ _ \| '__|
+| |__| | | | | (_| | | | | | | (_| | | |__| | | | | | |_| | | (_| | || (_) | |   
+\____/_| |_|_|\__, |_| |_| |_|\__,_| \____/_| |_| |_|\__,_|_|\__,_|\__\___/|_|   
+               __/ |                                                             
+              |___/                                                                                                                         """
+    print(presentation, "\n\n")
 
 
 def main():
     print_presentation()
     input("Press enter to enter configuration mode")
     enigma = get_configuration()
-    message = input("Enter message to encode or decode: ")
-    encoded_message = encode_message(message, enigma)
-    print("Encoded message:", encoded_message)
+    message = input("Enter message to encode or decode or '\\' to exit: ")
+    while message != "\\":
+        encoded_message = encode_message(message, enigma)
+        print("Encoded message:", encoded_message)
+        message = input("Enter message to encode or decode or '\\' to exit: ")
 
 
 if __name__ == "__main__":
